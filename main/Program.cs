8bc3f95bca0d2bbf684046bf42602b32e322a799
Ralph <s1gongche@163.com>
2021-11-09 15:08:29 +0800

    rename

--- main/Program.cs
+++ main/Program.cs
@@ -15,4 +15,36 @@ namespace api
             res.Main();
         }
     }
+
+    public class test
+    {
+        [Conditional("DEBUG")]
+        public void CheckState()
+        {
+            Console.WriteLine("debug success");
+        }
+
+        public void PrintOut()
+        {
+            CheckState();
+            //int[] foo = (from n in Enumerable.Range(0, 100)
+            //             select n * n).ToArray();
+
+            //foo.ForAll((n) => Console.WriteLine(n.ToString()));
+            Console.WriteLine("end");
+        }
+
+    }
+
+    public static class Exceptions
+    {
+        public static void ForAll<T>(
+            this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach (T item in sequence)
+            {
+                action(item);
+            }
+        }
+    }
 }
